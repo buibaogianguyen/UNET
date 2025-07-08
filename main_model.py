591d592d92b16adb54c2852b56f1a157e0cc25f5
buibaogianguyen <buibaogianguyen2008@gmail.com>
2025-07-08 21:06:27 +0700

    main model

--- main_model.py
+++ main_model.py
@@ -2,16 +2,27 @@ import torch
 import torch.nn as nn
 import torchvision.transforms.functional as TF
 
+class DepthWiseSeperableConv(nn.Module):
+    def __init__(self, in_channels, out_channels, kernel_size=3, padding=1):
+        super(DepthWiseSeperableConv, self).__init__()
+        self.depthwise = nn.Conv2d(in_channels, in_channels, kernel_size, padding=padding, groups=in_channels, bias=False)
+        self.pointwise = nn.Conv2d(in_channels, out_channels, 1, bias=False)
+
+    def forward(self, x):
+        x = self.depthwise(x)
+        x = self.pointwise(x)
+        return x  
+
 class DoubleConvolution(nn.Module):
     def __init__ (self, in_channels, out_channels):
         super(DoubleConvolution, self).__init__()
         self.conv = nn.Sequential(
             nn.Conv2d(in_channels, out_channels, 3, 1, 1, bias=False),
             nn.BatchNorm2d(out_channels),
-            nn.ReLU,
+            nn.ReLU(inplace=True),
             nn.Conv2d(out_channels, out_channels, 3, 1, 1, bias=False),
             nn.BatchNorm2d(out_channels),
-            nn.ReLU
+            nn.ReLU(inplace=True)
         )
 
     def forward(self, x):
@@ -20,5 +31,53 @@ class DoubleConvolution(nn.Module):
 class UNET(nn.Module):
     def __init__(self, in_channels=3, out_channels=1, features=[64,128,256,512]):
         super(UNET, self).__init__()
+        self.ups = nn.ModuleList()
+        self.downs = nn.ModuleList()
+        self.pool = nn.MaxPool2d(2, 2)
 
-        
+        # down part
+        for feature in features:
+            self.downs.append(DoubleConvolution(in_channels, feature))
+            in_channels = feature
+
+        # up part
+        for feature in reversed(features):
+            self.ups.append(nn.ConvTranspose2d(feature*2, feature,kernel_size=2, stride=2,))
+            self.ups.append(DoubleConvolution(feature*2, feature))
+
+        self.bottleneck = DoubleConvolution(features[-1], features[-1]*2)
+        self.final_conv = nn.Conv2d(features[0], out_channels, kernel_size=1)
+
+    def forward(self,x):
+        skip_connections = []
+
+        for down in self.downs:
+            x = down(x)
+            skip_connections.append(x)
+            x = self.pool(x)
+
+        x = self.bottleneck(x)
+        skip_connections = skip_connections[::-1]
+
+        for index in range(0, len(self.ups), 2):
+            x = self.ups[index](x)
+            skip_connection = skip_connections[index//2]
+
+            if x.shape != skip_connection.shape:
+                x = TF.resize(x, size=skip_connection.shape[2:])
+
+            concat_skip = torch.cat((skip_connection, x), dim=1)
+            x = self.ups[index+1](concat_skip)
+
+        return self.final_conv(x)
+
+def test():
+    x = torch.randn((3,1,160,160))
+    model = UNET(in_channels=1,out_channels=1)
+    preds = model(x)
+    print(preds.shape)
+    print(x.shape)
+    assert preds.shape == x.shape
+
+if __name__ == "__main__":
+    test()
